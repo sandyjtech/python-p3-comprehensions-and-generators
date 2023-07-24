81ef029ec80f4fa93ea65b921e81782bf15d1b42
sandyjtech <sandyjtech@gmail.com>
2023-07-24 07:21:37 -0700

    na

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+#new_list = [optional_operation(item) for item in old_list if optional_condition == True]
 
 def return_evens(num_list):
-    pass
-
+    return [x for x in num_list if x % 2 == 0]
+    
+   #Using a list comprehension, write a function make_exclamation() that takes a list of sentence strings 
+   #and returns a list of sentence strings with exclamation marks at the end.
 def make_exclamation(sentence_list):
-    pass
+    return [x + "!" for x in sentence_list]
